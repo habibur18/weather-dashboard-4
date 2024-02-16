c001597a08c95625e66f321fa07ea79d10488f9d
habibur18 <amarhabibur@gmail.com>
2024-02-16 07:43:25 +0600

    done application structure

---
+++ src/Components/Header/Favourite.tsx
@@ -0,0 +1,9 @@
+import favourite from "../../assets/heart.svg";
+export default function Favourite() {
+  return (
+    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+      <img src={favourite} alt="" />
+      <span>Favourite Locations</span>
+    </div>
+  );
+}
---
+++ src/Components/Header/Logo.tsx
@@ -0,0 +1,8 @@
+import logo from "../../assets/logo.svg";
+export default function Logo() {
+  return (
+    <a href="#">
+      <img src={logo} alt="" />
+    </a>
+  );
+}
