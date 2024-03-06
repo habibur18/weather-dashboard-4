8b1e860df536841fece70c388586c3fff3784a05
habibur18 <amarhabibur@gmail.com>
2024-03-06 13:53:51 +0600

    update api key

--- src/Components/Header/Search.tsx
+++ src/Components/Header/Search.tsx
@@ -25,7 +25,7 @@ export default function Search() {
     doSearch(value);
   };
   return (
-    <form>
+    <form onSubmit={(e) => e.preventDefault()}>
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input value={searchTerm} onChange={handleChange} className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none" type="search" placeholder="Search Location" required />
         <button type="submit">
